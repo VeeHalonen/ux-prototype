b7b00fadb5d13a346634086084d2aaf8d965fadb
VeeHalonen <veera.e.halonen@gmail.com>
2021-10-14 19:40:29 +0300

    Add Clearance Offers page

--- src/views/ClearanceItems.jsx
+++ src/views/ClearanceItems.jsx
@@ -1,5 +1,25 @@
+import { Button, Typography } from "@mui/material";
+import ProductThumbnail from "../components/ProductThumbnail";
+
 const ClearanceItems = () => {
-  return <div>Clearance Items</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Typography paragraph variant="h5" textAlign="start">
+        Clearance Offers
+      </Typography>
+      <div
+        style={{
+          marginTop: 20,
+          marginBottom: 30,
+        }}
+      >
+        {[...Array(20)].map((e, i) => {
+          return <ProductThumbnail key={i} discount={true} />;
+        })}
+      </div>
+      <Button variant="outlined">Load More</Button>
+    </div>
+  );
 };
 
 export default ClearanceItems;
