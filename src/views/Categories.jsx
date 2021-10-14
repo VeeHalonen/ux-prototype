8adaf9c58e024c0284225c6665735c9420d5a6aa
VeeHalonen <veera.e.halonen@gmail.com>
2021-10-14 19:37:45 +0300

    Add Categories page

--- src/views/Categories.jsx
+++ src/views/Categories.jsx
@@ -1,5 +1,24 @@
+import { Typography } from "@mui/material";
+import Category from "../components/Category";
+
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Typography paragraph variant="h5" textAlign="start">
+        Categories
+      </Typography>
+      <div
+        style={{
+          marginTop: 20,
+          marginBottom: 30,
+        }}
+      >
+        {[...Array(16)].map((e, i) => {
+          return <Category key={i} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
