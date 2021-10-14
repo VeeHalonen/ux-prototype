546298a90f4004f28bb0e82a7b444ee44ba137f2
VeeHalonen <veera.e.halonen@gmail.com>
2021-10-14 17:27:52 +0300

    Add About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,5 +1,35 @@
+import { Typography } from "@mui/material";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Typography variant="h5" paragraph>
+        Company
+      </Typography>
+      <Typography paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque nam
+        consequatur totam qui minus nostrum iste, veritatis ipsum, illo
+        voluptatem dignissimos suscipit sint architecto quia assumenda, quae
+        neque necessitatibus officiis.
+      </Typography>
+      <Typography paragraph>
+        Eius facere, earum quod quibusdam recusandae repellendus a et nostrum
+        molestias! Optio, id! Maxime, amet voluptate vitae perferendis harum
+        consectetur vel placeat, laudantium nostrum alias, debitis cum incidunt
+        aliquam tenetur.
+      </Typography>
+      <Typography variant="h5" paragraph>
+        Contact
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet,
+        <br />
+        consectetur, adipisicing elit.
+        <br />
+        Hic dicta repudiandae vitae.
+      </Typography>
+    </div>
+  );
 };
 
 export default About;
