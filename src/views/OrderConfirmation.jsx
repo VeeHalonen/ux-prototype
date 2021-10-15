137d53666e2e0ba0400192d8d5d26c863405053c
VeeHalonen <veera.e.halonen@gmail.com>
2021-10-15 08:52:53 +0300

    Add nav buttons

--- src/views/OrderConfirmation.jsx
+++ src/views/OrderConfirmation.jsx
@@ -1,7 +1,17 @@
 import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const OrderConfirmation = () => {
-  return <Typography variant="h5">Order Confirmation</Typography>;
+  return (
+    <div>
+      <Typography variant="h5">Order Confirmation</Typography>
+      <div style={{ textAlign: "end", marginTop: 30, marginBottom: 100 }}>
+        <Button component={Link} to="/" variant="contained">
+          Back to Home
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default OrderConfirmation;
--- src/views/ShippingAndPayment.jsx
+++ src/views/ShippingAndPayment.jsx
@@ -1,7 +1,26 @@
 import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ShippingAndPayment = () => {
-  return <Typography variant="h5">Shipping and Payment</Typography>;
+  return (
+    <div>
+      <Typography variant="h5">Shipping and Payment</Typography>
+      <div style={{ textAlign: "end", marginTop: 30, marginBottom: 100 }}>
+        <Button
+          component={Link}
+          to="/order-summary"
+          variant="contained"
+          color="secondary"
+          style={{ marginRight: 10 }}
+        >
+          Previous
+        </Button>
+        <Button component={Link} to="/order-confirmation" variant="contained">
+          Next
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ShippingAndPayment;
