b25e4f6f82604842cf8ff3d5329bac57b0d41741
VeeHalonen <veera.e.halonen@gmail.com>
2021-10-27 22:24:19 +0300

    Add banner images

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,61 +1,46 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const bannerStyle = {
-    width: "100%",
-    fontSize: 20,
-    background: "linear-gradient(to right, #607d8b, #a6a6a6, lightgray)",
-    marginBottom: 15,
-    padding: 40,
-    paddingLeft: 90,
-    paddingRight: 90,
-    textAlign: "start",
-  };
-  const categoryTitleStyle = {
-    fontWeight: "bold",
-    color: "#263238",
-  };
-  const linkStyle = {
-    textDecoration: "none",
-    color: "unset",
-  };
   return (
     <Grid
       container
-      item
       direction="column"
       textAlign="center"
       spacing={1}
       style={{ marginTop: 50 }}
-      xs={12}
-      md={10}
     >
       <Grid item>
-        <Link to="/clearance-items" style={linkStyle}>
-          <Card style={bannerStyle} elevation="none">
-            <Typography variant="h5" style={categoryTitleStyle}>
-              OFFERS
-            </Typography>
-          </Card>
+        <Link to="/clearance-items">
+          <img
+            src="bannerOffers.jpeg"
+            alt="Offers"
+            width="100%"
+            height="100%"
+            style={{ maxWidth: "1000px" }}
+          />
         </Link>
       </Grid>
       <Grid item>
-        <Link to="/search" style={linkStyle}>
-          <Card style={bannerStyle} elevation="none">
-            <Typography variant="h5" style={categoryTitleStyle}>
-              LATEST
-            </Typography>
-          </Card>
+        <Link to="/search">
+          <img
+            src="bannerLatest.jpeg"
+            alt="Latest"
+            width="100%"
+            height="100%"
+            style={{ maxWidth: "1000px" }}
+          />
         </Link>
       </Grid>
       <Grid item>
-        <Link to="/categories" style={linkStyle}>
-          <Card style={bannerStyle} elevation="none">
-            <Typography variant="h5" style={categoryTitleStyle}>
-              PRODUCT CATEGORIES
-            </Typography>
-          </Card>
+        <Link to="/categories">
+          <img
+            src="bannerCategories.jpeg"
+            alt="Categories"
+            width="100%"
+            height="100%"
+            style={{ maxWidth: "1000px" }}
+          />
         </Link>
       </Grid>
     </Grid>
