5b21847c389fda70ca3a01dc37ecb78f0150e4e8
VeeHalonen <veera.e.halonen@gmail.com>
2021-11-13 01:02:51 +0200

    Improve search filters

--- src/components/SearchOptions.jsx
+++ src/components/SearchOptions.jsx
@@ -12,16 +12,64 @@ import {
   Slider,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-// import {CATEGORIES, MATERIALS, COLORS, FEATURES, SIZES, POWER_CONSUMPTION, SOUND_LEVEL} from '../helpers'
+import {
+  CATEGORIES,
+  // MATERIALS,
+  // COLORS,
+  // FEATURES,
+  // SIZES,
+  // POWER_CONSUMPTION,
+  // SOUND_LEVEL,
+} from "../helpers";
 
 const SearchOptions = () => {
   const MAX_PRICE = 1500;
   const [textSearch, setTextSearch] = useState("");
   const [priceRange, setPriceRange] = useState([0, MAX_PRICE]);
+  const [category, setCategory] = useState("ALL");
   const handlePriceRangeChange = (event, newValue) => {
     setPriceRange(newValue);
   };
-
+  const title = (title) => {
+    return (
+      <Typography style={{ fontWeight: "bold", marginTop: 10 }}>
+        {title}
+      </Typography>
+    );
+  };
+  const rangeSlider = (value, min, max, onChange) => {
+    return (
+      <Slider
+        style={{ marginLeft: "10px", width: "90%" }}
+        value={value}
+        onChange={onChange}
+        valueLabelDisplay="off"
+        min={min}
+        max={max}
+      />
+    );
+  };
+  const select = (VALUES, value, onChangeFunction) => {
+    return (
+      <Select
+        value={value}
+        fullWidth
+        size="small"
+        onChange={(e) => {
+          onChangeFunction(e.target.value);
+        }}
+      >
+        <MenuItem value="ALL">ALL</MenuItem>
+        {VALUES.map((c) => {
+          return (
+            <MenuItem value={c} key={c}>
+              {c}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    );
+  };
   // const getFilters = () => {
   //   return {
   //     textSearch,
@@ -46,35 +94,21 @@ const SearchOptions = () => {
             <SearchIcon color="primary" />
           </Grid>
         </Grid>
-        <Grid item>
-          <Typography style={{ fontWeight: "bold", marginTop: 10 }}>
-            Filters
-          </Typography>
-        </Grid>
+        <Grid item>{title("Filters")}</Grid>
         <Grid item>
           <FormGroup>
             <FormControlLabel control={<Checkbox />} label="Option 1" />
             <FormControlLabel control={<Checkbox />} label="Option 2" />
           </FormGroup>
         </Grid>
-        <Grid item>
-          <Select value={10} fullWidth size="small">
-            <MenuItem value={10}>Option 1</MenuItem>
-            <MenuItem value={20}>Option 2</MenuItem>
-            <MenuItem value={30}>Option 3</MenuItem>
-          </Select>
-        </Grid>
+        <Grid item>{title("Category")}</Grid>
+        <Grid item>{select(CATEGORIES, category, setCategory)}</Grid>
         <Grid item>
           {/* Price Range */}
           <Typography>
             <b>Price: </b> {priceRange[0] + "-" + priceRange[1]} €
           </Typography>
-          <Slider
-            value={priceRange}
-            onChange={handlePriceRangeChange}
-            valueLabelDisplay="off"
-            max={MAX_PRICE}
-          />
+          {rangeSlider(priceRange, 0, MAX_PRICE, handlePriceRangeChange)}
           {/* TODO: */}
           <Typography>Range 2</Typography>
           <Slider defaultValue={50} valueLabelDisplay="auto" />
