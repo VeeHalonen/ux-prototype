cf42b0416ebaba1db7ff5446498cea17653a7226
VeeHalonen <veera.e.halonen@gmail.com>
2021-11-17 22:49:13 +0200

    Add cart items to order summary page

--- src/views/OrderSummary.jsx
+++ src/views/OrderSummary.jsx
@@ -1,11 +1,20 @@
 import { Grid, Typography, Card } from "@mui/material";
+import { useContext } from "react";
 import PrevNextButtons from "../components/PrevNextButtons";
 import ShoppingCartStepper from "../components/ShoppingCartStepper";
 import SimpleDataRow from "../components/SimpleDataRow";
 import SimpleDataRowTitle from "../components/SimpleDataRowTitle";
 import TotalSum from "../components/TotalSum";
+import {
+  getProductPrice,
+  getRandomProducts,
+  getTotalPrice,
+  GlobalStateContext,
+} from "../helpers";
 
 const OrderSummary = () => {
+  const context = useContext(GlobalStateContext);
+  const products = context?.globalState?.shoppingCart || getRandomProducts(2);
   return (
     <div>
       <ShoppingCartStepper step={1} />
@@ -15,10 +24,18 @@ const OrderSummary = () => {
         </Typography>
         <Grid container spacing={3} style={{ marginTop: 10 }}>
           <SimpleDataRowTitle />
-          <SimpleDataRow name="Product Name" price={50} quantity={1} />
-          <SimpleDataRow name="Product Name" price={50} quantity={1} />
+          {products.map((product, i) => {
+            return (
+              <SimpleDataRow
+                name={product.productName}
+                price={getProductPrice(product)}
+                quantity={1}
+                key={i}
+              />
+            );
+          })}
         </Grid>
-        <TotalSum sum={100} />
+        <TotalSum sum={getTotalPrice(products)} />
         <PrevNextButtons
           nextLink="/shipping-and-payment"
           prevLink="/shopping-cart"
