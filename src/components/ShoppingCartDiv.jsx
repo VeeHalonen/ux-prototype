4bf04cb4769912a3d8ef6b46e9aa15495e2b94e9
VeeHalonen <veera.e.halonen@gmail.com>
2021-12-08 19:17:56 +0200

    Add toasts to indicate language and currency features are not yet implemented

--- src/components/ShoppingCartDiv.jsx
+++ src/components/ShoppingCartDiv.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Select,
   FormControl,
@@ -8,10 +9,18 @@ import {
 } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
+import Toast from "./Toast";
 
 const ShoppingCartDiv = ({ count }) => {
+  const [toastMessage, setToastMessage] = useState("");
+  const [showToast, setShowToast] = useState(false);
   return (
     <Grid container alignItems="center" spacing={2} style={{ marginRight: 25 }}>
+      <Toast
+        message={toastMessage}
+        open={showToast}
+        onClose={() => setShowToast(false)}
+      />
       <Grid item>
         <Link to="/shopping-cart">
           <IconButton>
@@ -24,13 +33,27 @@ const ShoppingCartDiv = ({ count }) => {
       <Grid item>
         <Grid container item direction="column">
           <FormControl variant="outlined" size="small">
-            <Select size="small" defaultValue="en">
+            <Select
+              size="small"
+              defaultValue="en"
+              onChange={() => {
+                setToastMessage("Imagine the language changed!");
+                setShowToast(true);
+              }}
+            >
               <MenuItem value="en">EN</MenuItem>
               <MenuItem value="fi">FI</MenuItem>
             </Select>
           </FormControl>
           <FormControl variant="outlined" size="small">
-            <Select size="small" defaultValue="EUR">
+            <Select
+              size="small"
+              defaultValue="EUR"
+              onChange={() => {
+                setToastMessage("Imagine the currency changed!");
+                setShowToast(true);
+              }}
+            >
               <MenuItem value="EUR">€</MenuItem>
               <MenuItem value="USD">$</MenuItem>
               <MenuItem value="GBP">£</MenuItem>
