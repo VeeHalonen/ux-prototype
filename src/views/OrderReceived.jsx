63b5a6d27a8f02fcbaca1cb7d56b11a52eb856b7
VeeHalonen <veera.e.halonen@gmail.com>
2021-12-08 18:22:19 +0200

    Add random order numbers

--- src/views/OrderReceived.jsx
+++ src/views/OrderReceived.jsx
@@ -18,7 +18,7 @@ const OrderReceived = () => {
           Order Received!
         </Typography>
         <Typography paragraph style={{ fontWeight: "bold", marginBottom: 40 }}>
-          Order Number: #123456789
+          Order Number: #{Math.floor(Math.random() * 100000000)}
         </Typography>
         <Typography paragraph style={{ marginBottom: 40 }}>
           A confirmation has been sent to your email. Thank you for being a fan!
